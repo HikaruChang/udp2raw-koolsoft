92754f5f18e18a45d65764daccae91c5872c0433
Hikaru Chang <hikaru@joymoe.com>
2018-03-04 02:17:24 +0800

    20180225

--- config.json.js
+++ config.json.js
@@ -1,9 +1,9 @@
 {
-    "build_date": "2018-01-14_14:25:00", 
+    "build_date": "2018-03-04_02:16:00", 
     "description": "UDP流量突破隧道", 
     "home_url": "Module_udp2raw.asp", 
-    "md5": "51a0c690c599bd70e292fbd532d39ca3", 
+    "md5": "404e169ec01fa656ad2741bf1a3b111f", 
     "module": "udp2raw", 
     "title": "udp2raw", 
-    "version": "20180114"
+    "version": "20180225"
 }
